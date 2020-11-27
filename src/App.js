1a281e71e02184665f80425cc4a0dd5f7cbfdd3e
aditee-singh <singhaditee33@gmail.com>
2020-11-27 20:42:16 +0530

    Added middle section in home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home} />
         </Switch>
       </Router>
     </>
