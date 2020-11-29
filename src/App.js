70b8231c2770a8cbf525fe0cc41369f47e07d275
aditee-singh <singhaditee33@gmail.com>
2020-11-29 13:58:33 +0530

    Added styling in footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import './App.css';
 
 function App() {
   return (
